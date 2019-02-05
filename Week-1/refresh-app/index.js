6d5d6fb49bfe304bc5a2dd1c8cb92d304fbbbe30
Zafir Hasan <zafirhsn@gmail.com>
2019-02-05 00:28:49 -0500

    Refreshed on Dynamic Web topics

--- Week-1/refresh-app/index.js
+++ Week-1/refresh-app/index.js
@@ -4,9 +4,56 @@
 Create an app that displays the weather for cities based on the inputed route. Use Node.js, Express, a Weather API, EJS, and Bootstrap to create the app. 
 */
 
-const app = require("express")();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const request = require("request");
+const qs = require("querystring");
 const config = require("./config");
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = 8080;
+app.listen(port, ()=> {
+  console.log(`Server is listening on port ${port}`);
+})
 
 console.log(config.apikey);
 
-app.get("/")
+app.get('/', (req, res)=> {
+  res.render("city", {temp: "", city: ""});
+});
+
+app.post('/', (req, res)=> {
+  let regex = / /g;
+  req.body.city.replace(regex, '%20');
+
+  res.redirect(`/weather/${req.body.city}`);
+});
+
+app.get("/weather/:city", (req, res)=> {
+
+  let url = "http://api.openweathermap.org/data/2.5/weather?" +  
+    qs.stringify({
+      q: req.params.city==="tokyo" ? req.params.city + ",jp" : req.params.city + ",us",
+      appid: config.apikey
+    });
+
+    request.get(url, (err, response, body)=> {
+      if (err) {
+        console.log("There was an error!");
+        console.log(err);
+      }
+      else if (!err && response.statusCode === 200) {
+        let bodyJSON = JSON.parse(body);
+        console.log(bodyJSON);
+        res.render("city", {temp: ((bodyJSON.main.temp - 273.15) * (9/5) + 32), city: req.params.city});
+      }
+
+    });
+
+  // request.get("")
+
+});
