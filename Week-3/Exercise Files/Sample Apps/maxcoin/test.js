fb85390c008c0f27e18af97ddcf8ae1ef29144a6
Zafir Hasan <zafirhsn@gmail.com>
2019-04-01 23:55:36 -0400

    Progress

--- Week-3/Exercise Files/Sample Apps/maxcoin/test.js
+++ Week-3/Exercise Files/Sample Apps/maxcoin/test.js
@@ -1,5 +1,7 @@
 // request is a module that makes http calls easier
 const request = require('request');
+const MongoClient = require("mongodb").MongoClient;
+const dsn = "mongodb://localhost:37017/maxcoin";
 
 // Generic function that fetches the closing bitcoin dates of the last month from a public API
 function fetchFromAPI(callback) {
@@ -11,7 +13,33 @@ function fetchFromAPI(callback) {
     });
 }
 
-fetchFromAPI((err, data) => {
+function insertMongodb(collection, data) {
+    const promiseInserts = [];
+
+    Object.keys(data).forEach((key) => {
+        promiseInserts.push(
+            collection.insertOne({date: key, value: data[key]})
+        );
+    });
+    return Promise.all(promiseInserts);
+}
+
+MongoClient.connect(dsn), (err, db) => {
     if (err) throw err;
-    console.log(data);
-});
+    console.log("Connected successfully to MongoDB server");
+    fetchFromAPI((err, data) => {
+        if (err) throw err;
+        const collection = db.collection("value");
+        insertMongodb(collection, data.bpi).then((result) => {
+            console.log(`Succesfully inserted ${result.length} documents into mongodb`);
+            db.close();
+        }).catch((err) => {
+            console.log(err);
+            process.exit();
+        })
+    });
+    
+}
+
+
+
